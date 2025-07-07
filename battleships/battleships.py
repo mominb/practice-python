5ba913f6f6676a9a964bcbcea9bf5108f5aab6fe
Momin Babar <muhammad.m.babar@gmail.com>
2025-07-07 11:59:13 +0000

    Enhances game mechanics by refining attack logic, adding error handling, and improving user interface for input and grid display

--- battleships/battleships.py
+++ battleships/battleships.py
@@ -4,11 +4,14 @@ from grids import Grid
 
 
 class Game:
+    GRID_SIZE = 5
+
     def __init__(self) -> None:
-        self.grid = Game._make_grid()
+        self.grid = Grid(Game.GRID_SIZE, Game.GRID_SIZE)
         self._ships = Game._create_hidden_ships()
         self._ships_remaining = 3
-        self._tries = 10
+        self._tries = 11
+        self._attacked_positions = []
 
     @staticmethod
     def _create_hidden_ships():
@@ -21,37 +24,50 @@ class Game:
 
     @staticmethod
     def _create_a_hidden_ship():
-        x = random.randint(2, 6)
-        y = random.randint(1, 5)
+        x = random.randint(2, Game.GRID_SIZE)
+        y = random.randint(1, Game.GRID_SIZE - 1)
 
         return (x, y)
 
-    @staticmethod
-    def _make_grid():
-        grid = Grid(6, 6)
+    def attack_repeated(self, attack_pos):
+        if attack_pos in self._attacked_positions:
+            return True
+        else:
+            self._attacked_positions.append(attack_pos)
+            return False
 
-        for x in range(1, grid.width + 1):
-            for y in range(1, grid.height + 1):
-                if x == 1:
-                    grid.update_cell(x, y, 6 - y)
-                elif y == 6:
-                    grid.update_cell(x, y, x - 1)
-                else:
-                    grid.update_cell(x, y, ".")
-        return grid
+    @staticmethod
+    def is_attack_in_range(attack_pos):
+        (x, y) = attack_pos
+        if x <= 0 or y <= 0 or x > 5 or y > 5:
+            return False
+        else:
+            return True
 
     def attack(self, attack_pos):
-        for ship in self._ships:
-            if attack_pos == ship:
-                (x, y) = ship
-                self.grid.update_cell(x, y, "X")
-                self._ships_remaining -= 1
-                break
-            else:
-                (x, y) = attack_pos
-                self.grid.update_cell(x, y, "O")
+        if not Game.is_attack_in_range(attack_pos):
+            raise OutOfRangeError()
+        if self.attack_repeated(attack_pos):
+            raise DuplicateError()
+
+        if attack_pos in self._ships:
+            (x, y) = attack_pos
+            self.grid.update_cell(x, y, "X")
+            self._ships_remaining -= 1
+
+        else:
+            (x, y) = attack_pos
+            self.grid.update_cell(x, y, "O")
 
         self._tries -= 1
 
     def is_over(self):
         return self._tries == 0 or self._ships_remaining == 0
+
+
+class OutOfRangeError(Exception):
+    pass
+
+
+class DuplicateError(Exception):
+    pass
--- battleships/main.py
+++ battleships/main.py
@@ -1,18 +1,53 @@
-from .battleships import Game
+from .battleships import DuplicateError, Game, OutOfRangeError
 
 
 def main():
     game = Game()
 
     def get_attack_from_user():
-        print("Enter target position")
-        x = 1 + int(input("X-coordinates?  "))
-        y = 6 - int(input("Y-coordinates?  "))
+        print("\033[34mEnter target position\033[0m ")
+        print("_" * 30)
+        x = int(input("\033[34mX-coordinates? (1-5)\033[0m  "))
+        print("_" * 30)
+        y = int(input("\033[34mY-coordinates? (1-5)\033[0m  "))
+
         return x, y
 
+    def print_grid_and_intro(game: Game):
+        print("=" * 30)
+        print("\033[35mWELCOME TO BATTLESHIPS\033[0m")
+        print("\033[35mDestroy all three ships to win\033[0m")
+        print("=" * 30)
+        for X in range(game.GRID_SIZE + 1):
+            print(X, end="   ")
+        print()
+        for Y in range(1, Game.GRID_SIZE + 1):
+            print(f"{Y}", end="   ")
+            for cell in range(1, Game.GRID_SIZE + 1):
+                if game.grid.get_cell(cell, Y) == "∅":
+                    print(".", end="   ")
+                else:
+                    print(game.grid.get_cell(cell, Y), end="   ")
+
+            print()
+        print("=" * 30)
+
     while not game.is_over():
-        print(game.grid)
-        game.attack(get_attack_from_user())
+        print_grid_and_intro(game)
+        try:
+            game.attack(get_attack_from_user())
+        except OutOfRangeError:
+            print("\033[31mYou have entered out of range\033[0m")
+        except DuplicateError:
+            print("\033[31mYou have already attacked here\033[0m")
+        except ValueError:
+            print("\033[31mEnter numbers from 1-5 ONLY\033[0m")
+    if game.is_over():
+        if game._tries == 0:
+            print("Game Over!!! You lost")
+
+        else:
+            print("Congrats!!! You Won.")
 
 
 if __name__ == "__main__":
